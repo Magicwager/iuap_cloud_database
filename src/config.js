e03f1b6501eae9467a173883e702f3b76c493064
dangwei <dangwei@yonyou.com>
2017-12-05 19:30:37 +0800

    配置测试环境接口

--- src/config.js
+++ src/config.js
@@ -1,15 +1,18 @@
+// 不同的环境对应不同的接口
+//const serverURL = '/basedoc';
+const serverURL = '/bd';
 
 // 基础数据
 let Config = {
     // 币种
     currency: {
-        query: '/bd/currencys/list',             // 查询
-        add: '/bd/currencys/save',               // 新增
-        edit: '/bd/currencys/update',            // 编辑
-        delete: '/bd/currencys/delete',          // 删除
-        search: '/bd/currencys/search',          // 模糊搜索
-        setDefault: '/bd/currencys/setDefault',  // 设为默认
-        currencyRef: '/bd/currencys/refData'     // 币种参照
+        query: `${serverURL}/currencys/list`,             // 查询
+        add: `${serverURL}/currencys/save`,               // 新增
+        edit: `${serverURL}/currencys/update`,            // 编辑
+        delete: `${serverURL}/currencys/delete`,          // 删除
+        search: `${serverURL}/currencys/search`,          // 模糊搜索
+        setDefault: `${serverURL}/currencys/setDefault`,  // 设为默认
+        currencyRef: `${serverURL}/currencys/refData`     // 币种参照
     }
 };
 
